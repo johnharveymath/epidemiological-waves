15357b045226f68de3663488c259a5e73df1cd64
Alex Zarebski <aezarebski@gmail.com>
2021-09-14 18:41:03 +0100

    lint

--- manuscript-figures/figure-1-cases-and-deaths.R
+++ manuscript-figures/figure-1-cases-and-deaths.R
@@ -9,7 +9,7 @@ y <- read.csv("data/2020-09-13/fig1c-t0-epi-state.csv") %>%
   use_series(countrycode)
 
 x <- read.csv("data/2020-09-13/figure_5.csv") %>%
-  select(countrycode,date,new_per_day,dead_per_day) %>%
+  select(countrycode, date, new_per_day, dead_per_day) %>%
   filter(is.element(el = countrycode, set = y)) %>%
   mutate(date = as.Date(date)) %>%
   melt(id.vars = c("countrycode", "date"))
@@ -20,14 +20,16 @@ facet_labels <- c(
 )
 
 g <- ggplot(data = filter(x, countrycode != "USA"), mapping = aes(x = date, y = value)) +
- geom_line() +
- geom_smooth(method = "loess", span = 0.1) +
+  geom_line() +
+  geom_smooth(method = "loess", span = 0.1) +
   facet_wrap(~variable, scales = "free_y", labeller = labeller(variable = facet_labels)) +
- scale_y_sqrt() +
- scale_x_date(labels = label_date_short()) +
- labs(y = NULL, x = NULL) +
-  theme_classic()  +
-theme(strip.background = element_blank(),
-strip.text = element_text(face = "bold"))
+  scale_y_sqrt() +
+  scale_x_date(labels = label_date_short()) +
+  labs(y = NULL, x = NULL) +
+  theme_classic() +
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_text(face = "bold")
+  )
 
 ggsave(filename = "./output/png/figure-1-cases-and-deaths.png")
--- manuscript-figures/figure-1-t0-map.R
+++ manuscript-figures/figure-1-t0-map.R
@@ -9,8 +9,8 @@ library(geojsonio)
 
 ## read in the t_0 data to colour the regions with.
 t0_df <- read.csv(
-"data/2020-09-13/fig1a-t0-days.csv",
-stringsAsFactors = FALSE
+  "data/2020-09-13/fig1a-t0-days.csv",
+  stringsAsFactors = FALSE
 ) %>% rename(GID_0 = countrycode)
 
 
@@ -21,35 +21,42 @@ plot_sf <- left_join(world_sf, t0_df, by = "GID_0")
 ## choose some appropriate values for the colour scale
 t0_range <- range(purrr::discard(.x = plot_sf$days_to_t0, .p = is.na))
 my_breaks <- seq(from = t0_range[1], to = t0_range[2], length = 4)
-colours_hex <- c("#5e3c99",
-                 "#b2abd2",
-                 "#fdb863",
-                 "#e66101")
+colours_hex <- c(
+  "#5e3c99",
+  "#b2abd2",
+  "#fdb863",
+  "#e66101"
+)
 
 ## make the actual plot and do some preliminary styling.
 g <- ggplot() +
-  geom_sf(data = plot_sf,
-          mapping = aes(fill = days_to_t0),
-colour = "white",
-size = 0.1) +
-  scale_fill_gradientn(breaks = my_breaks,
-                       colors = colours_hex,
-                       limits = range(my_breaks)) +
+  geom_sf(
+    data = plot_sf,
+    mapping = aes(fill = days_to_t0),
+    colour = "white",
+    size = 0.1
+  ) +
+  scale_fill_gradientn(
+    breaks = my_breaks,
+    colors = colours_hex,
+    limits = range(my_breaks)
+  ) +
   labs(fill = "Days until epidemic\nthreshold reached") +
   theme_void() +
   theme(
-legend.box.background = element_rect(fill = "white", colour = "grey"),
-legend.box.margin = margin(t = 0.1, r = 0.2, b = 0.1, l = 0.2, unit = "cm"),
-legend.position = c(0.25, 0.15),
-legend.direction = "horizontal",
-legend.title = element_text(vjust = 1.0, size = 7),
-legend.key.height = unit(0.5, "line"),
-legend.text = element_text(size = 5)
-)
+    legend.box.background = element_rect(fill = "white", colour = "grey"),
+    legend.box.margin = margin(t = 0.1, r = 0.2, b = 0.1, l = 0.2, unit = "cm"),
+    legend.position = c(0.25, 0.15),
+    legend.direction = "horizontal",
+    legend.title = element_text(vjust = 1.0, size = 7),
+    legend.key.height = unit(0.5, "line"),
+    legend.text = element_text(size = 5)
+  )
 
-ggsave(filename = "./output/png/figure-1-t0-map.png",
-       plot = g,
-height = 10,
-width = 17,
-units = "cm"
+ggsave(
+  filename = "./output/png/figure-1-t0-map.png",
+  plot = g,
+  height = 10,
+  width = 17,
+  units = "cm"
 )
